ff4360c1dc7be32b8d7bc8da3c1298225370a77c
Ar-Razy Muhammad <arrazymuhammad@live.com>
2022-07-26 09:48:31 +0700

    update

---
+++ pbl_product.php
@@ -0,0 +1,288 @@
+<section class="all-item-area all-item-area-2 pd-top-100 pd-bottom-100">
+  <div class="container">
+    <div class="header-shadow-inner">
+      <div class="row">
+        <div class="col-lg-12 align-self-center">
+          <h2 class="text-center"> Product PBL</h2>
+        </div>
+      </div>
+    </div>
+    <div class="all-item-section">
+      <div class="item-isotope row">
+        <div class="item-sizer col-1"></div>
+        <div class="row">
+
+          <div class="all-isotope-item col-md-4">
+            <div class="thumb text-center">
+
+              <img src="assets/img/tampilan/simpkl.png" alt="image">
+
+              <a class="btn btn-white" style="margin-left:-30% ;" target="_blank" href="https://pbl.politap.ac.id/2022/preview/simpkl">Detail</a>
+
+              <a class="btn btn-white" style="margin-left:8% ;" target="_blank" href="https://pbl.politap.ac.id/2022/project/simpkl">Demo</a>
+            </div>
+            <div class="details" style="margin-top: 5%;">
+              <h4 class="text-center">SIMPKL</h4>
+              <p>Sistem Informasi Manajemen Praktek Kerja Lapangan</p>
+            </div>
+            <hr>
+          </div>
+
+          <div class="all-isotope-item col-md-4">
+            <div class="thumb text-center">
+              <img src="assets/img/tampilan/siabsensi.png" alt="image">
+
+              <a class="btn btn-white" style="margin-left:-30% ;" target="_blank" href="https://pbl.politap.ac.id/2022/preview/siabsensi">Detail</a>
+
+              <a class="btn btn-white" style="margin-left:8% ;" target="_blank" href="https://pbl.politap.ac.id/2022/project/siabsensi">Demo</a>
+            </div>
+            <div class="details" style="margin-top: 5%;">
+              <h4 class="text-center">SIABSENSI</h4>
+              <p>
+                Sistem Absensi Mahasiswa Program Studi D3 Teknologi Informasi
+              </p>
+              <hr>
+            </div>
+          </div>
+
+          <div class="all-isotope-item col-md-4">
+            <div class="thumb text-center">
+              <img src="assets/img/tampilan/siventalis.png" alt="image">
+
+              <a class="btn btn-white" style="margin-left:-30% ;" target="_blank" href="https://pbl.politap.ac.id/2022/preview/siventalis">Detail</a>
+
+              <a class="btn btn-white" style="margin-left:8% ;" target="_blank" href="https://pbl.politap.ac.id/2022/project/siventalis">Demo</a>
+            </div>
+            <div class="details" style="margin-top: 5%;">
+              <h4 class="text-center">SIVENTALIS</h4>
+              <p>
+                Sistem Informasi Inventarisir Alat dan Fasilitas Politeknik Negeri Ketapang
+              </p>
+              <hr>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+
+          <div class="all-isotope-item col-md-4">
+            <div class="thumb text-center">
+
+              <img src="assets/img/tampilan/sispi.png" alt="image">
+
+              <a class="btn btn-white" style="margin-left:-30% ;" target="_blank" href="https://pbl.politap.ac.id/2022/preview/sispi">Detail</a>
+
+              <a class="btn btn-white" style="margin-left:8% ;" target="_blank" href="https://pbl.politap.ac.id/2022/project/sispi">Demo</a>
+            </div>
+            <div class="details" style="margin-top: 5%;">
+              <h4 class="text-center">SISPI</h4>
+              <p>
+                Sistem Informasi Satuan Pengawasan Internal (SPI) Polteknik Negeri Ketapang
+              </p>
+            </div>
+            <hr>
+          </div>
+
+          <div class="all-isotope-item col-md-4">
+            <div class="thumb text-center">
+              <img src="assets/img/tampilan/sijala.png" alt="image">
+
+              <a class="btn btn-white" style="margin-left:-30% ;" target="_blank" href="https://pbl.politap.ac.id/2022/preview/sijala">Detail</a>
+
+              <a class="btn btn-white" style="margin-left:8% ;" target="_blank" href="https://pbl.politap.ac.id/2022/project/sijala">Demo</a>
+            </div>
+            <div class="details" style="margin-top: 5%;">
+              <h4 class="text-center">SIJALA</h4>
+              <p>
+                Sistem Informasi Peminjaman Laboratorium Politeknik Negeri Ketapang
+              </p>
+              <hr>
+            </div>
+          </div>
+
+          <div class="all-isotope-item col-md-4">
+            <div class="thumb text-center">
+              <img src="assets/img/tampilan/sipakpol.png" alt="image">
+
+              <a class="btn btn-white" style="margin-left:-30% ;" target="_blank" href="https://pbl.politap.ac.id/2022/preview/sipakpol">Detail</a>
+
+              <a class="btn btn-white" style="margin-left:8% ;" target="_blank" href="https://pbl.politap.ac.id/2022/project/sipakpol">Demo</a>
+            </div>
+            <div class="details" style="margin-top: 5%;">
+              <h4 class="text-center">SIPAKPOL</h4>
+              <p>
+                Sistem Informasi Pengajuan Angka Kredit Internal Politeknik Negeri Ketapang
+              </p>
+              <hr>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+
+          <div class="all-isotope-item col-md-4">
+            <div class="thumb text-center">
+
+              <img src="assets/img/tampilan/jne.jpeg" alt="image">
+
+              <a class="btn btn-white" style="margin-left:-30% ;" target="_blank" href="https://pbl.politap.ac.id/2022/preview/jneapp">Detail</a>
+
+              <a class="btn btn-white" style="margin-left:8% ;" target="_blank" href="https://pbl.politap.ac.id/2022/project/jneapp">Demo</a>
+            </div>
+            <div class="details" style="margin-top: 5%;">
+              <h4 class="text-center">JNEAPP</h4>
+              <p>
+                Sistem Digitalisasi Pelayanan JNE Kabupaten Ketapang
+              </p>
+            </div>
+            <hr>
+          </div>
+
+          <div class="all-isotope-item col-md-4">
+            <div class="thumb text-center">
+              <img src="assets/img/tampilan/sip3km.png" alt="image">
+
+              <a class="btn btn-white" style="margin-left:-30% ;" target="_blank" href="https://pbl.politap.ac.id/2022/preview/sip3km">Detail</a>
+
+              <a class="btn btn-white" style="margin-left:8% ;" target="_blank" href="https://pbl.politap.ac.id/2022/project/sip3km">Demo</a>
+            </div>
+            <div class="details" style="margin-top: 5%;">
+              <h4 class="text-center">SIP3KM</h4>
+              <p>
+                Sistem Informasi Penelitian dan Pengabdian kepada Masyarakat Politeknik Negeri Ketapang
+              </p>
+              <hr>
+            </div>
+          </div>
+
+          <div class="all-isotope-item col-md-4">
+            <div class="thumb text-center">
+              <img src="assets/img/tampilan/lsp.png" alt="image">
+
+              <a class="btn btn-white" style="margin-left:-30% ;" target="_blank" href="https://pbl.politap.ac.id/2022/preview/silsp">Detail</a>
+
+              <a class="btn btn-white" style="margin-left:8% ;" target="_blank" href="https://pbl.politap.ac.id/2022/project/silsp">Demo</a>
+            </div>
+            <div class="details" style="margin-top: 5%;">
+              <h4 class="text-center">SI LSP P-1 POLITAP</h4>
+              <p>
+                Sistem Informasi Lembaga Sertifikasi Profesi Pihak Pertama Politeknik Negeri Ketapang
+              </p>
+              <hr>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+
+          <div class="all-isotope-item col-md-4">
+            <div class="thumb text-center">
+
+              <img src="assets/img/tampilan/siami.png" alt="image">
+
+              <a class="btn btn-white" style="margin-left:-30% ;" target="_blank" href="https://pbl.politap.ac.id/2022/preview/siami">Detail</a>
+
+              <a class="btn btn-white" style="margin-left:8% ;" target="_blank" href="https://pbl.politap.ac.id/2022/project/siami">Demo</a>
+            </div>
+            <div class="details" style="margin-top: 5%;">
+              <h4 class="text-center">SIAMI</h4>
+              <p>
+                Sistem Informasi Audit Mutu Internal Politeknik Negeri Ketapang
+              </p>
+            </div>
+            <hr>
+          </div>
+
+          <div class="all-isotope-item col-md-4">
+            <div class="thumb text-center">
+              <img src="assets/img/tampilan/mydata.png" alt="image">
+
+              <a class="btn btn-white" style="margin-left:-30% ;" target="_blank" href="https://pbl.politap.ac.id/2022/preview/mydata">Detail</a>
+
+              <a class="btn btn-white" style="margin-left:8% ;" target="_blank" href="https://pbl.politap.ac.id/2022/project/mydata">Demo</a>
+            </div>
+            <div class="details" style="margin-top: 5%;">
+              <h4 class="text-center">MYDATA POLITAP</h4>
+              <p>
+                Sistem Informasi Pelayanan Kepegawaian Politeknik Negeri Ketapang
+              </p>
+              <hr>
+            </div>
+          </div>
+
+          <div class="all-isotope-item col-md-4">
+            <div class="thumb text-center">
+              <img src="assets/img/tampilan/sipedi.png" alt="image">
+
+              <a class="btn btn-white" style="margin-left:-30% ;" target="_blank" href="https://pbl.politap.ac.id/2022/preview/sipedi">Detail</a>
+
+              <a class="btn btn-white" style="margin-left:8% ;" target="_blank" href="https://pbl.politap.ac.id/2022/project/sipedi">Demo</a>
+            </div>
+            <div class="details" style="margin-top: 5%;">
+              <h4 class="text-center">SIPEDI</h4>
+              <p>
+                Sistem Informasi Perpustakaan Digital Politeknik Negeri Ketapang
+              </p>
+              <hr>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="all-isotope-item col-md-4">
+            <div class="thumb text-center">
+
+              <img src="assets/img/tampilan/sipa.png" alt="image">
+
+              <a class="btn btn-white" style="margin-left:-30% ;" target="_blank" href="https://pbl.politap.ac.id/2022/preview/sipa">Detail</a>
+
+              <a class="btn btn-white" style="margin-left:8% ;" target="_blank" href="https://pbl.politap.ac.id/2022/project/sipa">Demo</a>
+            </div>
+            <div class="details" style="margin-top: 5%;">
+              <h4 class="text-center">SIPA</h4>
+              <p>
+                Sistem Informasi Pelayanan Akademik Politeknik Negeri Ketapang
+              </p>
+            </div>
+            <hr>
+          </div>
+
+          <div class="all-isotope-item col-md-4">
+            <div class="thumb text-center">
+              <img src="assets/img/tampilan/sihaki.png" alt="image">
+
+              <a class="btn btn-white" style="margin-left:-30% ;" target="_blank" href="https://pbl.politap.ac.id/2022/preview/sihaki">Detail</a>
+
+              <a class="btn btn-white" style="margin-left:8% ;" target="_blank" href="https://pbl.politap.ac.id/2022/project/sihaki">Demo</a>
+            </div>
+            <div class="details" style="margin-top: 5%;">
+              <h4 class="text-center">SIHAKI</h4>
+              <p>
+                Sistem Informasi Pengajuan Hak Kekayaan Intelektual (HKI) Politeknik Negeri Ketapang (Sihaki)
+              </p>
+              <hr>
+            </div>
+          </div>
+
+          <div class="all-isotope-item col-md-4">
+            <div class="thumb text-center">
+              <img src="assets/img/tampilan/siyafri.png" alt="image">
+
+              <a class="btn btn-white" style="margin-left:-30% ;" target="_blank" href="https://pbl.politap.ac.id/2022/preview/siyafri">Detail</a>
+
+              <a class="btn btn-white" style="margin-left:8% ;" target="_blank" href="https://pbl.politap.ac.id/2022/project/siyafri">Demo</a>
+            </div>
+            <div class="details" style="margin-top: 5%;">
+              <h4 class="text-center">SIYAFRI</h4>
+              <p>
+                Sistem Informasi Website Yayasan Amfibi Reptil Indonesia
+              </p>
+              <hr>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</section>
